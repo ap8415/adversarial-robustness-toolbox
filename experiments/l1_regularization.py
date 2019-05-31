aff981064d0f3624cf64dd21f2e20c11303bcff4
Andrei <ap8415@imperial.ac.uk>
2019-05-31 13:23:20 +0100

    Adding l1/l2 experiments

--- experiments/l1_regularization.py
+++ experiments/l1_regularization.py
@@ -18,16 +18,16 @@ from experiment_models import neural_networks
 
 print(x_train.shape)
 
-dropout_levels = [0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.55, 0.6, 0.65, 0.7]
+l1_reg_levels = [0.005, 0.01, 0.015, 0.02, 0.025, 0.03]
 
-for dropout in range(0, 10):
-    classifier = neural_networks.three_layer_dnn(x_train.shape[1:], 300, 100, dropout_levels[dropout], 0, 0)
+for l1_level in range(0, 6):
+    classifier = neural_networks.three_layer_dnn(x_train.shape[1:], 300, 100, 0, l1_reg_levels[l1_level], 0)
     classifier.fit(x_train, y_train, nb_epochs=10, batch_size=128)
 
     # Evaluate the classifier on the test set
     preds = np.argmax(classifier.predict(x_test), axis=1)
     acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
-    print("\nTest accuracy on dropout level %.2f%%: %.2f%%" % (dropout_levels[dropout], acc * 100))
+    print("\nTest accuracy on L1 regularization level %.2f%%: %.2f%%" % (l1_reg_levels[l1_level], acc * 100))
 
     # Craft adversarial samples with CW attack
     adv_crafter = CarliniL2Method(classifier, targeted=False)
@@ -36,7 +36,8 @@ for dropout in range(0, 10):
     # Evaluate the classifier on the adversarial examples
     preds = np.argmax(classifier.predict(x_test_adv), axis=1)
     acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
-    print("\nTest accuracy on adversarial sample for dropout %.2f%%: %.2f%%" % (dropout_levels[dropout], acc * 100))
+    print("\nTest accuracy on adversarial sample for L1 regularization level %.2f%%: %.2f%%"
+          % (l1_reg_levels[l1_level], acc * 100))
 
     # Calculate the average perturbation in L1 and L2 norms. Note that I don't de-normalize the values.
     perturbations = np.absolute((x_test_adv - x_test[:1000]))
@@ -44,8 +45,8 @@ for dropout in range(0, 10):
     l2_perturbations = [LA.norm(perturbation, 2) for perturbation in perturbations]
     avg_l1_perturbation = np.average(l1_perturbations)
     avg_l2_perturbation = np.average(l2_perturbations)
-    print("\nAverage L1-norm perturbation from Carlini L2 attack for dropout %.2f%%: %.2f%%"
-          % (dropout_levels[dropout], avg_l1_perturbation))
-    print("\nAverage L2-norm perturbation from Carlini L2 attack for dropout %.2f%%: %.2f%%"
-          % (dropout_levels[dropout], avg_l2_perturbation))
+    print("\nAverage L1-norm perturbation from Carlini L2 attack for L1 regularization level %.2f%%: %.2f%%"
+          % (l1_reg_levels[l1_level], avg_l1_perturbation))
+    print("\nAverage L2-norm perturbation from Carlini L2 attack for L1 regularization level %.2f%%: %.2f%%"
+          % (l1_reg_levels[l1_level], avg_l2_perturbation))
 
