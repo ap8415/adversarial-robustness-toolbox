714ea70acdfbd6bb880d98b8d8f8cfb63e28a2d7
Andrei-Bogdan Puiu <ap8415@firecrest.doc.ic.ac.uk>
2019-06-11 22:14:22 +0100

    some more things

--- l1_reg.sh
+++ l1_reg.sh
@@ -1,4 +1,4 @@
-#PBS -l nodes=gpu01 -l mem=8GB -l gpu_mem=8GB
+#PBS -l nodes=gpu15 -l mem=6GB -l gpu_mem=5GB
 
 cd /vol/gpudata/ap8415/adversarial-robustness-toolbox 
 
@@ -8,7 +8,6 @@ export LD_LIBRARY_PATH=/vol/cuda/9.0.176/lib64:/vol/cuda/9.0.176/lib:$LD_LIBRARY
 
 source activate venv
 
-python3 experiments/adversarial_examples_heatmap.py six_layer_dnn carlini_l2
 python3 experiments/adversarial_examples_heatmap.py six_layer_dnn carlini_lInf
 
 
