4d76e1c013305196c2bd197c865c2770b94ef930
Andrei-Bogdan Puiu <ap8415@firecrest.doc.ic.ac.uk>
2019-06-14 21:50:52 +0100

    Adding l-inf, transferability results + lenet5 h5 model

--- transferability_1.sh
+++ transferability_1.sh
@@ -8,5 +8,4 @@ export LD_LIBRARY_PATH=/vol/cuda/9.0.176/lib64:/vol/cuda/9.0.176/lib:$LD_LIBRARY
 
 source activate venv
 
-python3 experiments/transferability_experiment.py three_layer_dn
-
+python3 experiments/transferability_experiment.py three_layer_dnn
