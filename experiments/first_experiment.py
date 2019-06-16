8a6ff92307b936361b5d1817896028ab4cb9e520
Andrei <ap8415@imperial.ac.uk>
2019-06-16 23:38:22 +0100

    adding Foolbox experiment

--- experiments/first_experiment.py
+++ experiments/first_experiment.py
@@ -22,30 +22,32 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 np.set_printoptions(threshold=sys.maxsize)
 
-
-classifier = neural_networks.three_layer_dnn(x_train.shape[1:], 300, 100, 0, 0, 0)
-
-
-classifier.fit(x_train, y_train, nb_epochs=10, batch_size=128)
-
-# Evaluate the classifier on the test set
-preds = np.argmax(classifier.predict(x_test), axis=1)
-acc = (np.sum(preds == np.argmax(y_test, axis=1)) / len(y_test)) * 100
-
-# Craft adversarial samples with CW attack
-
-attacker = CarliniLInfMethod(classifier,
-                             targeted=True)
-
-x_real = x_test[:1]
-y_real = np.argmax(y_test[:1], axis=1)
-x_test_adv = attacker.generate(x_real, np.array([to_one_hot(1)]))
-print(x_real[0] - x_test_adv[0])
-print('PULA')
-print(classifier.predict(x_real, logits=True))
-print(classifier.predict(x_test_adv, logits=True))
-print(classifier.predict(x_real))
-print(classifier.predict(x_test_adv))
+print(y_test[0])
+#
+#
+# classifier = neural_networks.three_layer_dnn(x_train.shape[1:], 300, 100, 0, 0, 0)
+#
+#
+# classifier.fit(x_train, y_train, nb_epochs=10, batch_size=128)
+#
+# # Evaluate the classifier on the test set
+# preds = np.argmax(classifier.predict(x_test), axis=1)
+# acc = (np.sum(preds == np.argmax(y_test, axis=1)) / len(y_test)) * 100
+#
+# # Craft adversarial samples with CW attack
+#
+# attacker = CarliniLInfMethod(classifier,
+#                              targeted=True)
+#
+# x_real = x_test[:1]
+# y_real = np.argmax(y_test[:1], axis=1)
+# x_test_adv = attacker.generate(x_real, np.array([to_one_hot(1)]))
+# print(x_real[0] - x_test_adv[0])
+# print('PULA')
+# print(classifier.predict(x_real, logits=True))
+# print(classifier.predict(x_test_adv, logits=True))
+# print(classifier.predict(x_real))
+# print(classifier.predict(x_test_adv))
 
 
 
---
+++ foolbox_exp/experiment.py
@@ -0,0 +1,75 @@
+from foolbox.models import KerasModel
+import keras.backend as k
+from keras import regularizers
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, AveragePooling2D
+from foolbox.batch_attacks import CarliniWagnerL2Attack
+from foolbox.criteria import Misclassification
+from foolbox.distances import Linf
+
+from art.utils import load_mnist
+
+(x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
+# Pad images to 32x32 size in order to fit the LeNet/VGG architectures
+x_train = np.pad(x_train, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+x_test = np.pad(x_test, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+
+# model = Sequential()
+#
+# model.add(Conv2D(6, (3, 3), activation='relu', input_shape=(32, 32, 1)))
+# model.add(AveragePooling2D())
+# model.add(Dropout(0.3))
+# model.add(Conv2D(16, (3, 3), activation='relu'))
+# model.add(AveragePooling2D())
+# model.add(Dropout(0.3))
+#
+# model.add(Flatten())
+#
+# model.add(Dense(units=120, activation='relu'))
+# model.add(Dropout(0.3))
+# model.add(Dense(units=84, activation='relu'))
+# model.add(Dropout(0.3))
+#
+# model.add(Dense(units=10, activation='softmax'))
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+import time
+
+print(time.time())
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 1)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.3))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.3))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dropout(0.3))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(x_train, y_train, verbose=0, epochs=20, batch_size=128)
+
+kmodel = KerasModel(model=model, bounds=(0, 1))
+
+attack = CarliniWagnerL2Attack(kmodel, Misclassification())
+
+adversarial = attack(x_test[:1000], np.argmax(y_test[:1000], axis=1), binary_search_steps=10, max_iterations=200)
+
+preds = np.argmax(model.predict(adversarial), axis=1)
+
+acc = (np.sum(preds == np.argmax(y_test[:1000], axis=1)) / len(y_test)) * 100
+print(acc)
+
+print(time.time())
