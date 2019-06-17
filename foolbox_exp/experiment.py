a6f89553da59079a0bd913c12f950d6f4e074c92
Andrei <ap8415@imperial.ac.uk>
2019-06-17 20:11:57 +0100

    final foolbox xp for modelwide dropout

--- foolbox_exp/experiment.py
+++ foolbox_exp/experiment.py
@@ -53,8 +53,24 @@ import time
 
 print("Current time: %.2f" % time.time())
 
-# dr = [0, 0.25, 0.5, 0.65]
 dr = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85]
+
+l1_std = []
+l2_std = []
+linf_std = []
+
+l1_normalized = []
+l2_normalized = []
+linf_normalized = []
+
+l1_misclas = []
+l2_misclas = []
+linf_misclas = []
+
+failure_rate = []
+
+linear_mmd = []
+
 for dropout in dr:
     kmodel = None
 
@@ -69,36 +85,55 @@ for dropout in dr:
     # elif args.experiment_type == "leNet5":
     #     classifier = convolutional.leNet_cnn_single(dropout_levels[dropout])
 
-    # kmodel.fit(x_train, y_train, epochs=1, batch_size=128)
-    kmodel.fit(x_train, y_train, epochs=20, batch_size=128)
+    # kmodel.fit(x_train, y_train, epochs=10, batch_size=128)
+    kmodel.fit(x_train, y_train, epochs=50, batch_size=128)
+
+    preds = np.argmax(kmodel.predict(x_test), axis=1)
 
     attack = CarliniWagnerL2Attack(kmodel, Misclassification())
 
-    # adversarial = attack(x_test[:10], np.argmax(y_test[:10], axis=1), binary_search_steps=5, max_iterations=600)
-    adversarial = attack(x_test[:1000], np.argmax(y_test[:1000], axis=1), binary_search_steps=5, max_iterations=600)
+    # x_sample = x_test[:10]
+    # y_sample = y_test[:10]
+    x_sample = x_test[:1000]
+    y_sample = y_test[:1000]
+
+    adversarial = attack(x_sample, np.argmax(y_sample, axis=1), binary_search_steps=5, max_iterations=600)
 
     # For those samples for which the L2 method does not produce an adversarial sample within the attack parameters,
-    # we exclude them from the perturbation evaluation. the None given by the attack with the original input.
+    # we exclude them from the perturbation evaluation.
 
     failed = 0
+    misclassified = 0
+
     perturbations = []
     adv_examples = []
     orig_examples = []
     correct_labels = []
+
+    adv_examples_no_misclassification = []
+    orig_examples_no_misclassification = []
 
     orig_examples_failed = []
     correct_labels_failed = []
     for i in range(0, len(adversarial)):
         # If the attack fails, an array of NaN's is returned. We check for this here.
         if not any([np.isnan(x) for x in adversarial[i].flatten()]):
-            perturbations.append(np.abs(adversarial[i] - x_test[i]))
+            perturbations.append(np.abs(adversarial[i] - x_sample[i]))
             adv_examples.append(adversarial[i])
-            orig_examples.append(x_test[i])
-            correct_labels.append(y_test[i])
+            orig_examples.append(x_sample[i])
+            correct_labels.append(y_sample[i])
+
+            # We count the misclassified samples as well, as those are returned as adversarials equal to the input
+            # image. All misclassified samples will reach this if-branch by definition of the adversarial attack.
+            if np.argmax(y_sample[i]) != preds[i]:
+                misclassified += 1
+            else:
+                adv_examples_no_misclassification.append(x_sample[i])
+                orig_examples_no_misclassification.append(adversarial[i])
         else:
             failed += 1
-            orig_examples_failed.append(x_test[i])
-            correct_labels_failed.append(y_test[i])
+            orig_examples_failed.append(x_sample[i])
+            correct_labels_failed.append(y_sample[i])
 
     print('Initially failed: %d' % failed)
 
@@ -118,41 +153,76 @@ for dropout in dr:
             # Since it failed on the second try, we abandon trying to compute an adversarial sample for this run.
             if not any([np.isnan(x) for x in adversarial_strong[i].flatten()]):
                 perturbations.append(np.abs(adversarial_strong[i] - orig_examples_failed[i]))
-                adv_examples.append(adversarial[i])
-                orig_examples.append(x_test[i])
+                adv_examples.append(adversarial_strong[i])
+                orig_examples.append(orig_examples_failed[i])
                 correct_labels.append(correct_labels_failed[i])
+
+                adv_examples_no_misclassification.append(x_sample[i])
+                orig_examples_no_misclassification.append(adversarial_strong[i])
+
                 failed -= 1
 
     print('Failed after the 2nd attack attempt: %d' % failed)
+    failure_rate.append(failed)
 
     adv_examples = np.array(adv_examples)
     correct_labels = np.array(correct_labels)
     orig_examples = np.array(orig_examples)
     perturbations = np.array(perturbations)
 
-    print("Failed attacks: %d" %
-          np.sum(np.argmax(kmodel.predict(adv_examples), axis=1) == np.argmax(correct_labels, axis=1)))
-    print("Size of perturbation vectors: %d" % len(perturbations))
+    orig_examples_no_misclassification = np.array(orig_examples_no_misclassification)
+    adv_examples_no_misclassification = np.array(adv_examples_no_misclassification)
 
     l1_perturbations = [LA.norm(perturbation.flatten(), 1) for perturbation in perturbations]
     l2_perturbations = [LA.norm(perturbation.flatten(), 2) for perturbation in perturbations]
     lInf_perturbations = [LA.norm(perturbation.flatten(), np.inf) for perturbation in perturbations]
-    linear_mmd_real_vs_adversarial = mmd_evaluation(orig_examples, adv_examples)
+    linear_mmd_real_vs_adversarial = mmd_evaluation(orig_examples_no_misclassification,
+                                                    adv_examples_no_misclassification)
 
-    print('Average l1, l2, linf perturbations over the successful attacks:')
-    print(np.average(l1_perturbations))
-    print(np.average(l2_perturbations))
-    print(np.average(lInf_perturbations))
+    l1_std.append(np.average(l1_perturbations))
+    l2_std.append(np.average(l2_perturbations))
+    linf_std.append(np.average(lInf_perturbations))
 
     # We normalize by the coefficient of failed attacks to incorporate failure rate into our security measure.
     # For example, if 50% of our attacks fail, we multiply by a coeficient of (1/ (50/100)) = 1/ (1/2) = 2.
-    success_coef = ((10 - failed) / 10)
-    # success_coef = ((1000 - failed) / 1000)
-    normalization_factor = 1 / success_coef  # Note: no div-by-0 check as there can't be 1000 failed attacks.
+    # success_coef = ((10 - failed) / 10)
+    # Note: no div-by-0 check as there can't be len(x_sample) failed attacks unless there's a bug in the implementation.
+    attack_success_coef = 1 / ((len(x_sample) - failed) / len(x_sample))
 
-    print('Average l1, l2, linf normalized perturbations over the successful attacks:')
-    print(np.average(l1_perturbations) * normalization_factor)
-    print(np.average(l2_perturbations) * normalization_factor)
-    print(np.average(lInf_perturbations) * normalization_factor)
+    l1_normalized.append(np.average(l1_perturbations) * attack_success_coef)
+    l2_normalized.append(np.average(l2_perturbations) * attack_success_coef)
+    linf_normalized.append(np.average(lInf_perturbations) * attack_success_coef)
+
+    # We discount the misclassified samples in this computation.
+    misclassified_coef = 1 / ((len(x_sample) - misclassified) / len(x_sample))
+    l1_misclas.append(np.average(l1_perturbations) * attack_success_coef * misclassified_coef)
+    l2_misclas.append(np.average(l2_perturbations) * attack_success_coef * misclassified_coef)
+    linf_misclas.append(np.average(lInf_perturbations) * attack_success_coef * misclassified_coef)
+
+    linear_mmd.append(linear_mmd_real_vs_adversarial)
 
     print("Current time: %.2f" % time.time())
+
+print('Average l1, l2, linf perturbations over the attacks:')
+print(l1_std)
+print(l2_std)
+print(linf_std)
+
+print('Average l1, l2, linf normalized perturbations wrt failed attacks over the successful attacks:')
+print(l1_normalized)
+print(l2_normalized)
+print(linf_normalized)
+
+# We discount the misclassified samples in this computation.
+print('Average l1, l2, linf normalized perturbations wrt failed and misclassified over the successful attacks:')
+print(l1_misclas)
+print(l2_misclas)
+print(linf_misclas)
+
+print('No of failures in attack after 2 attempts:')
+print(failure_rate)
+
+print('MMD computation for real vs adversarial samples:')
+print(linear_mmd)
+print('And in log-scale:')
+print([np.math.log(x) for x in linear_mmd])
