0d4d5cf19673f8a50b8b33a1f8f1877ba4be31eb
Andrei-Bogdan Puiu <ap8415@firecrest.doc.ic.ac.uk>
2019-06-18 14:25:35 +0100

    Adding current set of results

--- l1_foolbox_xp2.sh
+++ l1_foolbox_xp2.sh
@@ -8,4 +8,4 @@ export LD_LIBRARY_PATH=/vol/cuda/9.0.176/lib64:/vol/cuda/9.0.176/lib:$LD_LIBRARY
 
 source activate venv
 
-python3 foolbox_exp/experiment.py six_layer_dnn
+python3 foolbox_exp/experiment_l1reg.py six_layer_dnn
