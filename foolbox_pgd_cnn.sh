6545db1ea483f5d997cf4e762b8c906f2d154a69
Andrei <ap8415@imperial.ac.uk>
2019-06-19 23:26:20 +0100

    pgd vgg mnist untargeted

--- foolbox_pgd_cnn.sh
+++ foolbox_pgd_cnn.sh
@@ -8,5 +8,4 @@ export LD_LIBRARY_PATH=/vol/cuda/9.0.176/lib64:/vol/cuda/9.0.176/lib:$LD_LIBRARY
 
 source activate venv
 
-python3 foolbox_exp/experiment.py leNet5
 python3 foolbox_exp/experiment.py VGG
